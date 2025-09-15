c9977f8f3051050d6a462dec5e2d0c0c981e6aa3
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-09-15 20:37:07 +0430

    first commit

---
+++ 01-MERN-Store-Project/backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import productRoute from './routes/product.route.js';
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+app.use(express.json())
+
+app.use('/api/products', productRoute)
+
+app.listen(PORT, () => {
+    connectDB()
+    console.log("Server is running on port 5000");
+})
