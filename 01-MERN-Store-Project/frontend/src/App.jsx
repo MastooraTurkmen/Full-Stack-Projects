7ca61a588ca411a6cbf027a6bb654cb3f39809e0
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-09-16 20:42:58 +0430

    add, delete, update products

--- 01-MERN-Store-Project/frontend/src/App.jsx
+++ 01-MERN-Store-Project/frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import { Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import { useColorModeValue } from "@chakra-ui/react";
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   );
 }
 
