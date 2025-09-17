06c584538429c79c37fe7f90f17acf12286af2dd
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-09-17 14:52:56 +0430

    deployment

--- 01-MERN-Store-Project/backend/server.js
+++ 01-MERN-Store-Project/backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoute from './routes/product.route.js';
 
@@ -7,11 +8,22 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve()
+
 app.use(express.json())
 
 app.use('/api/products', productRoute)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get("/*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
+
 app.listen(PORT, () => {
     connectDB()
-    console.log("Server is running on port 5000");
+    console.log(`Server is running on port ${PORT}...`);
 })
