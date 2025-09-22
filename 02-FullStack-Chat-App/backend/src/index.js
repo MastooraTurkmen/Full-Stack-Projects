9d384f157f17a2a4093954c7627f389b6400f2f8
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-09-22 16:23:56 +0430

    connect to db

--- 02-FullStack-Chat-App/backend/src/index.js
+++ 02-FullStack-Chat-App/backend/src/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv'
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from './lib/db.js';
 
+dotenv.config()
 const app = express()
+
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+    connectDB()
 })
